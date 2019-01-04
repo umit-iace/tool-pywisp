3bae81896d04e91f0c6e8f233e9130120665c04f
Jens <wurm.jens@gmail.com>
2019-01-04 01:11:27 +0100

    Add periodic task for control loop in tcp example server

--- examples/tcp/server/Transport.h
+++ examples/tcp/server/Transport.h
@@ -1,48 +1,28 @@
-#ifndef TRANSPORT_CLASS_H
-#define TRANSPORT_CLASS_H
+//
+// Created by Jens Wurm on 04.01.19.
+//
 
-#include "../Comm.h"
-/** \file Transport.h
- *  \class Transport
- *  \brief Klasse zum Ent- und Packen der Kommunikationsdaten zu Pywisp
- *
- *  Die Klasse wird mit zwei Buffer initialisiert, einen Eingangsbuffer und
- *  einen Ausgangsbuffer. Diese Buffer müssen die gleiche Länge besitzen wie
- *  die auf Pywisp. Will man mehrere Frames schicken, so können weitere Buffer
- *  über die addOutBuffer() Methode hinzugefügt werden. Damit diese auch
- *  gesendet werden, muss entsprechender Frame in sendData() gepackt werden.
- *  Eingangsseitig gibt es nur einen Buffer der die Frames liefert. Mit
- *  der Methode run() kann kontrolliert werden, ob Eingangsseitig Daten
- *  ausgewertet werden müssen. Mittels runExp() kann kontrolliert werden
- *  ob ein Experiment ausgeführt werden soll.
- */
+#ifndef TRANSPORT_H
+#define TRANSPORT_H
 
+#include "utils.h"
 
+class Transport {
+public:
+    bool runExp() { return this->bActivateExperiment; }
 
+    void sendData();
 
-class Transport: public Comm{
-	private:
-	Comm *tcp;
-	/**< Unpack Frame nach entsprechender ID*/
-	void unpackExp(unsigned char *payload);
-	/**< Flag ob Experiment aktiviert oder nicht*/
-	bool bActivateExperiment;
-	/**< Zählvariablen und Cursoren zum Bufferschreiben*/
-	unsigned char framecounter;
-	unsigned char frame;
-	unsigned char obuf_cur;
-	unsigned char ibuf_cur;
+    /**
+     * @brief struct of test rig data
+     */
+    struct benchData {
+        unsigned long lTime = 0;                    ///< milliseconds since start of experiment
+        bool bReset = false;                        ///< reset flag for new experiment start
+    } _benchData;
 
-	public:
-	/**< Rückgabe des Experimenten Flags*/
-	bool runExp() {return this->bActivateExperiment; }
-	/**< Senden der Daten in Frames*/
-	void sendData();
-	/**< Funktion zum Auswerten eingegangener Daten*/
-	void handleFrame(unsigned char id, unsigned char buf[MAX_PAYLOAD]);
-	void registerServer(Comm *serv);
-
-	unsigned long lTime;
+    bool bActivateExperiment = true;
 };
 
-#endif //TRANSPORT_CLASS_H
+
+#endif //TRANSPORT_H
--- examples/tcp/server/main.cpp
+++ examples/tcp/server/main.cpp
@@ -1,90 +1,27 @@
-#define _BUR_USE_DECLARATION_IN_IEC
-#ifdef _DEFAULT_INCLUDES
-	#include "AsDefault.h"
-#endif
-#include "../Libraries/TCPServer/TCPServer.h"
-#include "../Libraries/EventLog/EventLog.h"
-#include "../Libraries/Transport/Transport.h"
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
-/**< amount of memory to be allocated for heap storage must be specified for
-     every ANSI C++ program with the bur_heap_size variable */
-unsigned long bur_heap_size = 0xFFFFF;
+#include "PeriodicTask.h"
+#include "Transport.h"
 
-/**< Zählervariablen*/
-int counter_close = 0;
-int counter_tcp = 0;
-/**< Instanz des Eventloggers*/
-EventLogger EvLog_1((unsigned long) &strlogbook, &EvLogInstance_1);
-/**< Instanz der Tcp Server Klasse*/
-TCPServer TCPS_0(50007, &TcpServer_instance_0,
-				&EvLog_1);
+const unsigned long lDt = 10;          ///< Sampling step [s]
 
-/**< Instanz der Transport Klasse*/
 Transport transport;
 
-void _INIT ProgramInit(void)
-{
-	Main_state = 0;
-	counter_close = 0;
-	counter_tcp = 0;
-	TCPS_0.registerListener(&transport);
-	transport.registerServer(&TCPS_0);
-	TCPS_0.deinit();
-}
-
-void _CYCLIC ProgramCyclic(void)
-{
-    switch (Main_state)
-    {
-        case 0:
-            /**< Starte Server und warte auf Client*/
-            TCPS_0.init();
-            if (TCPS_0.status == TCPServer::READY)
-                Main_state = 1;
-            else if ((TCPS_0.status == TCPServer::STOP) || (TCPS_0.status == TCPServer::ERROR))
-                Main_state = 255;
-            break;
-        case 1:
-            /**< Lese und Schreibe mit Client*/
-            TCPS_0.sync();
-            if (TCPS_0.status == TCPServer::READY)
-                Main_state = 1;
-            else if ((TCPS_0.status == TCPServer::STOP) || (TCPS_0.status == TCPServer::ERROR))
-                Main_state = 10;
-            break;
-        case 10:
-            /**< Schließe Verbindung*/
-            TCPS_0.close_sockets();
-            if (TCPS_0.status == TCPServer::READY)
-                Main_state = 11;
-            break;
-        case 11:
-            /**< Warte 5 Sekunden und starte Server erneut*/
-            if (counter_close > 500)
-            {
-                counter_close = 0;
-                Main_state = 0;
-            }
-            else
-                counter_close += 1;
-            break;
-        default:
-            break;
+void fContLoop() {
+    if (transport.runExp()) {
+        transport._benchData.lTime += lDt;
+        transport.sendData();
     }
-    /**< Sende alle 100 ms Messdaten zu Pywisp*/
-	if (transport.runExp()) {
-    	if (counter_tcp >= 10) {
-            transport.sendData();
-            transport.lTime += 100;
-			counter_tcp = 0;
-		} else {
-			counter_tcp += 1;
-        }
-	}
 }
 
-void _EXIT ProgramExit(void)
+
+int main(int argc, char const *argv[])
 {
-	// Insert code here
-    TCPS_0.close_sockets();
+    PeriodicScheduler scheduler;
+    scheduler.addTask("fContLoop", boost::bind(fContLoop), 1);
+
+    scheduler.run();
+
+    return 0;
 }
