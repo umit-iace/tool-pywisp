9fe9361a52624ff2569b86285f99b12a1d5ba730
Jens Wurm <jens.wurm@umit.at>
2022-02-09 20:11:23 +0100

    Add udp connection

--- pywisp/connection.py
+++ pywisp/connection.py
@@ -15,7 +15,7 @@ from PyQt5 import QtCore
 
 from . import MINSerial, MINFrame
 
-__all__ = ["Connection", "TcpConnection", "SerialConnection"]
+__all__ = ["Connection", "UdpConnection", "TcpConnection", "SerialConnection"]
 
 
 class Connection(object):
@@ -249,6 +249,110 @@ class TcpConnection(Connection, QtCore.QThread):
                 for i in range(self.payloadLen + 1 - len(outputData)):
                     outputData += b'\x00'
             self.sock.send(outputData)
+        except Exception as e:
+            self._logger.error("Writing to host not possible! {}".format(e))
+            self.isConnected = False
+
+class UdpConnection(Connection, QtCore.QThread):
+    """
+    A connection derived Class for a udp client implemented as a QThread, which sends and get messages from test rig
+    """
+    def __init__(self,
+                 ip,
+                 port,
+                 payloadLen=80,
+                 timeout=0.01):
+        super(UdpConnection, self).__init__()
+        QtCore.QThread.__init__(self)
+
+        self.ip = ip
+        self.port = port
+        self.sock = None
+        self.payloadLen = payloadLen
+        self.timeout = timeout
+        self.moveToThread(self)
+
+    def disconnect(self):
+        self.isConnected = False
+        time.sleep(1)
+        if self.sock is not None:
+            self.sock.close()
+        self._reset()
+
+    def clear(self):
+        self._reset()
+
+    def _reset(self, ):
+        pass
+
+    def connect(self):
+        """
+        Checks if the given port is available and instantiates the socket connection
+        :return: True if successful connected, False otherwise.
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            self.sock.bind((self.ip, int(self.port)))
+        except socket.error:
+            self._logger.error("Connection to the server is not possible!")
+            self.sock.close()
+            self.sock = None
+            return False
+        self.isConnected = True
+        self.sock.settimeout(self.timeout)
+        return True
+
+    def getIP(self):
+        """
+        Gets the IP address of host and returns it
+        :return: the IP address
+        """
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        return ip
+
+    def run(self):
+        """
+        Endless loop of the thread
+        """
+        while self.isConnected:
+            if self.doRead:
+                self.readData(None)
+            else:
+                time.sleep(self.timeout)
+
+    def readData(self, frames):
+        """
+        Reads a data frame from the socket connection, builds a min frame and emits it.
+        :param frames: nothing
+        """
+        try:
+            data = self.sock.recvfrom(self.payloadLen + 1)
+            if data and data != b'':
+                if len(data) != self.payloadLen + 1:
+                    self._logger.error(
+                        "Length of data {} differs from payload length {}!".format(len(data), self.payloadLen + 1))
+                else:
+                    frame = MINFrame(data[0], data[1:], 0, False)
+                    self.received.emit(frame)
+        except socket.timeout:
+            # if nothing is to read, get on
+            pass
+        except socket.error as e:
+            self._logger.error("Reading from host not possible! {}".format(e))
+            self.isConnected = False
+
+    def writeData(self, data):
+        """
+        Writes a data frame to the socket connection
+        :param data: data frame to be sent
+        """
+        try:
+            outputData = struct.pack('>B', data['id']) + data['msg']
+            if len(outputData) < self.payloadLen + 1:
+                for i in range(self.payloadLen + 1 - len(outputData)):
+                    outputData += b'\x00'
+            self.sock.sendto(outputData, (self.ip, self.port))
         except Exception as e:
             self._logger.error("Writing to host not possible! {}".format(e))
             self.isConnected = False
