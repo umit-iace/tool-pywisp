9526ffe8a2bf7bb32e2ead04fc79745b4b33bc8a
Jonathan Halmen <slowjo@halmen.xyz>
2024-11-06 14:26:09 +0100

    example/generic/server: update for latest tool-libs changes
    - use <sys/comm.h> instead of deprecated <linux/host.h>
    - ExpLog is part of experiment now
    - trajectory returns list instead of value
    - SeriesUnpacker is now a template
    - no need for external 'ncat' anymore
    
    fixes #141

--- examples/generic/server/settings.py
+++ examples/generic/server/settings.py
@@ -18,5 +18,5 @@ pendulum2Mass = 0.1                         # kg
 pendulum1Length = 0.25                      # m
 pendulum2Length = 0.25                      # m
 
-pendulum1Inertia = 4.0 / 3.0 * pendulum1Mass * pendulum1Length ** 2  
-pendulum2Inertia = 4.0 / 3.0 * pendulum2Mass * pendulum2Length ** 2 
+pendulum1Inertia = 4.0 / 3.0 * pendulum1Mass * pendulum1Length ** 2
+pendulum2Inertia = 4.0 / 3.0 * pendulum2Mass * pendulum2Length ** 2
