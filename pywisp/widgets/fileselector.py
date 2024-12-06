4b86b02dc6571444c28c15be65706ff964b47e5b
Jonathan Halmen <slowjo@halmen.xyz>
2024-12-06 10:01:43 +0100

    FileSelector: start logging, be smarter about file extensions
    it is now unnecessary to explicitly provide the file extension, if not
    given, the active filter is used to correctly name the file.
    Additionally unused extensions are now rejected

--- pywisp/widgets/fileselector.py
+++ pywisp/widgets/fileselector.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import QFileDialog
+import logging
 import os
+import re
 from .utils import settings
 
 _default = {
@@ -14,23 +16,38 @@ class FileSelector(QFileDialog):
         super().__init__()
         self.st = settings(_default)
         self.formats = formats
+        self.exts = [self._extFromFilter(flt) for flt in formats]
+        self.logger = logging.getLogger("FileSelector")
+
+    def _extFromFilter(self, flt):
+        return re.search(r'\*([.a-z]*)', flt).group(1)
 
     def getSaveFileName(self, text="Export as ..."):
         path = self.st.value("path/export_dir")
         ext = self.st.value("path/export_ext")
-        file = os.path.join(path, "export" + ext)
+        file = os.path.join(path, "export")
         filterStr = ";;".join(self.formats)
         ix = [ ext in fmt for fmt in self.formats ].index(True)
 
-        selection = super().getSaveFileName(self, text, file, filterStr, self.formats[ix])
-        if not (filename := selection[0]):
+        selpath, selfilt = super().getSaveFileName(self, text, file,
+                                                   filterStr, self.formats[ix])
+        if not selpath:
+            self.logger.warn("File selection aborted!")
             return
-        selpath = os.path.dirname(filename)
+        name, selext = os.path.splitext(selpath)
+        if not selext:
+            selext = self._extFromFilter(selfilt)
+            selpath += selext
+        elif selext not in selfilt:
+            self.logger.error(f"Extension '{selext}' doesn't match used filter. Aborting!")
+            return
+
+        seldir = os.path.dirname(selpath)
         if path != selpath:
-            self.st.setValue("path/export_dir", selpath)
-        _, selext = os.path.splitext(filename)
+            self.st.setValue("path/export_dir", seldir)
+
         if ext != selext:
             self.st.setValue("path/export_ext", selext)
 
-        return filename
+        return selpath
 
