3c58a63279cc1aa7619d75e8bf441f09832f4441
Jens Wurm <jens.wurm@umit.at>
2024-12-10 01:50:51 +0100

    Include traj to visu, rename connection, datapoint handling

--- examples/generic/visu/connection.py
+++ examples/generic/visu/connection.py
@@ -3,10 +3,11 @@ from collections import OrderedDict
 from pywisp.connection import *
 
 
-class ConnTestTCP(UdpConnection):
-    settings = OrderedDict([("ip", '127.0.0.1'),
-                            ("port", '45670'),
-                            ])
+class Connection(UdpConnection):
+    settings = {
+        "ip", '127.0.0.1',
+        "port", '45670',
+    }
 
     def __init__(self):
         super().__init__(**self.settings)
