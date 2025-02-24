d99b31b344e2b9c453851ac13d7f4537d1d5e6d5
Jonathan Halmen <slowjo@halmen.xyz>
2025-02-24 12:39:37 +0100

    connection: rework incoming min stream unpacking
    this is more correct, handles error cases better, and is able to recover
    from them

--- pywisp/min.py
+++ pywisp/min.py
@@ -62,32 +62,61 @@ def Packer(sink):
         sink.send( bytes(ret) )
 
 @coroutine
+def HDRStuf(sink):
+    """ Handle HDR and STF bytes of incoming stream """
+    while True:
+        b1 = (yield)
+        if b1 != HDR:
+            sink.send( (b1, True) )
+            continue
+        b2 = (yield)
+        if b2 != HDR:
+            sink.send( (b1, True) )
+            sink.send( (b2, True) )
+            continue
+        b3 = (yield)
+        if b3 == STF:
+            sink.send( (b1, True) )
+            sink.send( (b2, True) )
+            continue
+        elif b3 == HDR:
+            sink.send( (0, False) )
+            continue
+        else:
+            # something has gone wrong, give up
+            sink.send( (0, False) )
+
+@coroutine
 def Unpacker(sink):
     """ unpack received data from wire into Frame. coroutine """
     while True:
-        if (yield)!= HDR:
+        id, ok = (yield)
+        if not ok: continue
+        if id >= 64: # not handling transport frames
+            # print(f"dropping transport frame: {id=}")
             continue
-        if (yield)!= HDR:
-            continue
-        if (yield)!= HDR:
-            continue
-        id = (yield)
-        assert(id < 64)
-        len = (yield)
-        hdcnt = 0
+        len, ok = (yield)
+        if not ok: continue
         pld = bytearray([])
         for _ in range(len):
-            n = (yield)
+            n, ok = (yield)
+            if not ok: break
             pld.append(n)
-            if n == HDR:
-                hdcnt += 1
-                if hdcnt == 2:
-                    (yield)
-                    hdcnt = 0
-            else:
-                hdcnt = 0
-        crc = bytes([(yield),(yield),(yield),(yield)])
+        if not ok: continue
+        c1, ok = (yield)
+        if not ok: continue
+        c2, ok = (yield)
+        if not ok: continue
+        c3, ok = (yield)
+        if not ok: continue
+        c4, ok = (yield)
+        if not ok: continue
+        crc = bytes([c1,c2,c3,c4])
         want = crc32(bytes([id, len])+pld)
-        if spack('>I',want) != crc:
+        if spack('>I',want) != crc: # dropping frame
+            # print(f"dropping frame mismatched CRC {id=}")
             continue
         sink.send( (id, bytes(pld) ))
+        eof, ok = (yield)
+        if eof != STF: # expected EOF==STF, but don't care
+            pass
