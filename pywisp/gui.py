umit-iace/tool-pywisp

commit 8512bcdef446343ec3424a2e03852fb4959aacea

incremental improvements (#134)
* simplify experiment interactor

* registry overhaul

* cap max framerate for visu

* set parent for GUI dialogs

this makes the dialogs appear as children of the main application, as
they should.

* crude IACEConnection implementation

* fix typos

* handle connections differently in gui

* disconnect: prevent infinite recursion

* fix GUI timer sometimes wrongly staying enabled

* change directory to main file upon starting

this allows starting visualizations from anywhere, i.e.
`python path/to/visu/main.py`

pywisp/gui.py
@@ -35,12 +35,12 @@ from PyQt5.QtWidgets import *
 from pyqtgraph import PlotWidget, TextItem
 from pyqtgraph.dockarea import *
 
-from .connection import SerialConnection, TcpConnection, UdpConnection
+from .connection import SerialConnection, SocketConnection, IACEConnection
 from .experiments import ExperimentInteractor, ExperimentView
 from .registry import *
 from .utils import getResource, PlainTextLogger, DataPointBuffer, PlotChart, Exporter, DataIntDialog, \
     DataTcpIpDialog, RemoteWidgetEdit, FreeLayout, MovablePushButton, MovableSwitch, MovableSlider, PinnedDock, \
-    ContextLineEditAction, TreeWidgetStyledItemDelegate
+    ContextLineEditAction, TreeWidgetStyledItemDelegate, IACEConnDialog
 
 from .visualization import MplVisualizer, VtkVisualizer
 from .gamepad import getGamepadByIndex
@@ -354,24 +354,28 @@ class MainGui(QMainWindow):
 
         availableConns = getRegisteredConnections()
         if availableConns:
-            for cls, name in availableConns:
-                self._logger.info("Found Connection: {}".format(name))
-                self.connections[cls] = {}
+            for name, cls in availableConns.items():
+                self._logger.info(f"Found Connection: {name}")
+                self.connections[name] = {'cls':cls}
         else:
             self._logger.error("No Connections found, return!")
             # return
 
         serialCnt = 0
-        for conn, connInstance in self.connections.items():
-            if issubclass(conn, SerialConnection):
-                serialMenu = self.connMenu.addMenu(conn.__name__)
-                self._getSerialMenu(serialMenu, conn.settings)
-                if conn.settings['port'] == '':
-                    self.setDefaultComPort(conn.settings, serialCnt)
+        for name, conn in self.connections.items():
+            cls = conn['cls']
+            if issubclass(cls, SerialConnection):
+                serialMenu = self.connMenu.addMenu(name)
+                self._getSerialMenu(serialMenu, cls.settings)
+                if cls.settings['port'] == '':
+                    self.setDefaultComPort(cls.settings, serialCnt)
                 serialCnt += 1
-            elif issubclass(conn, TcpConnection) or issubclass(conn, UdpConnection) :
-                actTcp = self.connMenu.addAction(conn.__name__)
-                actTcp.triggered.connect(lambda _, settings=conn.settings: self._getTcpMenu(settings))
+            elif issubclass(cls, IACEConnection):
+                actTcp = self.connMenu.addAction(name)
+                actTcp.triggered.connect(lambda _, settings=conn.settings: self._getIACEMenu(settings))
+            elif issubclass(cls, SocketConnection):
+                actTcp = self.connMenu.addAction(name)
+                actTcp.triggered.connect(lambda _, settings=cls.settings: self._getTcpMenu(settings))
             else:
                 self._logger.warning("Cannot handle the connection type!")
             self.connMenu.addSeparator()
@@ -457,16 +461,21 @@ class MainGui(QMainWindow):
             self.animationLayout.itemAt(i).widget().setParent(None)
 
         visName = self.visComboBox.itemText(idx)
-        availableVis = getRegisteredVisualizers()
+        available = getRegisteredVisualizers()
 
-        for aVis in availableVis:
-            if aVis[1] == visName:
-                self.setVisualizer(aVis[0])
-                break
+        self.setVisualizer(available[visName])
+
+    def _getIACEMenu(self, settings):
+        data = IACEConnDialog.getData(parent=self,**settings)
+        if data:
+            ip, port = data
+            print(f"got {ip=} from Dialog")
+            settings['ip'] = ip
+            settings['port'] = port
 
     def _getTcpMenu(self, settings):
         # ip and port
-        ip, port, ok = DataTcpIpDialog.getData(ip=settings['ip'], port=settings['port'])
+        ip, port, ok = DataTcpIpDialog.getData(parent=self,ip=settings['ip'], port=settings['port'])
 
         if ok:
             settings['ip'] = ip
@@ -613,7 +622,7 @@ class MainGui(QMainWindow):
         Sets the timer time in settings with a dialog.
         """
         self._settings.beginGroup('plot')
-        timerTime, ok = DataIntDialog.getData(title="Timer Time", min=2, max=10000, unit='ms',
+        timerTime, ok = DataIntDialog.getData(parent=self,title="Timer Time", min=2, max=10000, unit='ms',
                                               current=self.config['TimerTime'])
 
         if ok:
@@ -1138,13 +1147,14 @@ class MainGui(QMainWindow):
         self.lastMeasList.scrollToItem(item)
         self.copyLastMeas(item)
 
-        self.timer.start(int(self.config['TimerTime']))
+        self.timer.start(max(int(self.config['TimerTime']), 40)) # ~25 fps should be plenty
         if self.config['HeartbeatTime']:
             self.heartbeatTimer.start(int(self.config['HeartbeatTime']))
         self.exp.runExperiment()
 
-        self.guiStartTime = time.time()
-        self.guiTimer.start(1000)
+        if self.actStopExperiment.isEnabled():
+            self.guiStartTime = time.time()
+            self.guiTimer.start(1000)
 
     @pyqtSlot()
     def stopExperiment(self):
@@ -1280,10 +1290,7 @@ class MainGui(QMainWindow):
         if availableVis and 'Visu' in self._experiments[idx]:
             if self._experiments[idx]['Visu'] is not None:
                 for vis in self._experiments[idx]['Visu']:
-                    for avis in availableVis:
-                        if vis == avis[1]:
-                            used.append(avis)
-                            break
+                    used.append(availableVis[vis])
             else:
                 self._logger.warning("No Visualization configured!")
                 self.visualizer = None
@@ -1294,10 +1301,10 @@ class MainGui(QMainWindow):
         self.visComboBox.clear()
         self.visComboBox.disconnect()
         for vis in used:
-            self.visComboBox.addItem(vis[1])
+            self.visComboBox.addItem(vis.__name__)
         self.visComboBox.currentIndexChanged.connect(self.visualizerChanged)
-        self._logger.info("loading visualizer '{}'".format(used[0][1]))
-        self.setVisualizer(used[0][0])
+        self._logger.info("loading visualizer '{}'".format(used[0].__name__))
+        self.setVisualizer(used[0])
 
     def setVisualizer(self, vis):
         if issubclass(vis, MplVisualizer):
@@ -1417,22 +1424,22 @@ class MainGui(QMainWindow):
         """
         Connects all connections and sets the button states.
         """
-        for conn, _ in self.connections.items():
-            connInstance = conn()
+        for name, conn in self.connections.items():
+            connInstance = conn['cls']()
             if connInstance.connect():
-                self._logger.info("Connection for {} established!".format(conn.__name__))
+                self._logger.info(f"Connection for {name} established!")
                 self.actConnect.setEnabled(False)
                 self.actDisconnect.setEnabled(True)
                 if self._currentExpListItem is not None and self.selectedExp:
                     self.actStartExperiment.setEnabled(True)
                 self.actStopExperiment.setEnabled(False)
                 self.statusbarLabel.setText("Connected!")
-                connInstance.received.connect(lambda frame, conn=conn: self.updateData(frame, conn))
+                connInstance.received.connect(lambda frame: self.updateData(frame, name))
                 connInstance.finished.connect(self.disconnect)
-                self.connections[conn] = connInstance
+                self.connections[name]['inst'] = connInstance
                 self.isConnected = True
             else:
-                self._logger.warning("No connection for {} established! Check your settings!".format(conn.__name__))
+                self._logger.warning(f"No connection for {name} established! Check your settings!")
                 self.isConnected = False
                 return
 
@@ -1441,11 +1448,11 @@ class MainGui(QMainWindow):
         PySignal function, that sends the given data to the connections
         :param data: to send data
         """
-        for conn, connInstance in self.connections.items():
-            if connInstance and data['id'] == 1:
-                connInstance.writeData(data)
-            elif connInstance and data['connection'] == conn.__name__:
-                connInstance.writeData(data)
+        if data['id'] == 1:
+            for conn in self.connections.values():
+                conn['inst'].writeData(data)
+        else:
+            self.connections[data['connection']]['inst'].writeData(data)
 
     @pyqtSlot()
     def disconnect(self):
@@ -1455,10 +1462,10 @@ class MainGui(QMainWindow):
         if self.actStopExperiment.isEnabled():
             self.stopExperiment()
 
-        for conn, connInstance in self.connections.items():
-            if connInstance:
-                connInstance.disconnect()
-                self.connections[conn] = None
+        for conn in self.connections.values():
+            if conn.get('inst', None) and conn['inst'].connected:
+                conn['inst'].disconnect()
+                del conn['inst']
         self.actConnect.setEnabled(True)
         self.actDisconnect.setEnabled(False)
         self.actStartExperiment.setEnabled(False)
@@ -1488,9 +1495,8 @@ class MainGui(QMainWindow):
         dataPoints = data['DataPoints']
         names = data['DataPoints'].keys()
 
-        for key, value in self._currentDataPointBuffers.items():
-            if key in names:
-                value.addValue(time, dataPoints[key])
+        for key in names:
+            self._currentDataPointBuffers[key].addValue(time, dataPoints[key])
 
         time_text = "Exp time={}".format(timeString(time))
         self.expTimeLabel.setText(time_text)

pywisp/__init__.py
@@ -27,3 +27,8 @@ with open(getResource("logging.yaml", ""), "r") as f:
     log_conf = yaml.load(f, Loader=Loader)
 
 logging.config.dictConfig(log_conf)
+
+# go to correct directory
+import sys
+if (dir := os.path.dirname(sys.argv[-1])):
+    os.chdir(dir)

pywisp/connection.py
@@ -13,7 +13,7 @@ from PyQt5.QtCore import QObject, QThread, pyqtSignal
 from .min import Packer, Unpacker, Frame, Bytewise
 from .utils import coroutine, pipe
 
-__all__ = ["Connection", "UdpConnection", "TcpConnection", "SerialConnection"]
+__all__ = ["Connection", "UdpConnection", "TcpConnection", "SerialConnection", "IACEConnection"]
 
 
 class ConnReader(QObject):
@@ -240,3 +240,7 @@ class UdpConnection(SocketConnection):
     def __init__(self, ip, port, **kwargs):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         super().__init__(sock, ip, port, tx=Packer, rx=[Bytewise, Unpacker], **kwargs)
+
+class IACEConnection(UdpConnection):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)

pywisp/experimentModules.py
@@ -55,14 +55,14 @@ class ExperimentModule(QObject, metaclass=ExperimentModuleMeta):
         pass
 
     @abstractmethod
-    def getStartParams(self, *kwargs):
+    def getStartParams(self, *args):
         pass
 
     @abstractmethod
-    def getStopParams(self, *kwargs):
+    def getStopParams(self, *args):
         pass
 
     @abstractmethod
-    def getParams(self, *kwargs):
+    def getParams(self, *args):
         pass
 

pywisp/experiments.py
@@ -161,11 +161,8 @@ class ExperimentInteractor(QObject):
         :param moduleName: the given experiment module name
         :return: settings of module or raise an exception if module is unknown
         """
-        for module in getRegisteredExperimentModules():
-            if module[1] == moduleName:
-                return module[0].publicSettings
-
-        raise ExperimentException("_readSettings(): No module called {} found!".format(moduleName))
+        modules = getRegisteredExperimentModules()
+        return modules[moduleName].publicSettings
 
     def _readDataPoints(self, moduleName):
         """
@@ -173,11 +170,8 @@ class ExperimentInteractor(QObject):
         :param moduleName: the given experiment module name
         :return: data points of module or raise an exception if module is unknown
         """
-        for module in getRegisteredExperimentModules():
-            if module[1] == moduleName:
-                return module[0].dataPoints
-
-        raise ExperimentException("_readDataPoints(): No module called {} found!".format(moduleName))
+        modules = getRegisteredExperimentModules()
+        return modules[moduleName].dataPoints
 
     def itemChanged(self, item):
         """
@@ -367,17 +361,12 @@ class ExperimentInteractor(QObject):
             self._logger.warn("rig missed heartbeat! disconnecting...")
             self.missedbeat.emit()
             return None
-        for row in range(self.targetModel.rowCount()):
-            index = self.targetModel.index(row, 0)
-
-            parent = index.model().itemFromIndex(index)
-            moduleName = parent.data(role=PropertyItem.RawDataRole)
-
-            for module in getRegisteredExperimentModules():
-                if module[1] == moduleName and module[0].connection == connection.__name__:
-                    dataPoints = module[0].handleFrame(module[0], frame)
-                    if dataPoints is not None:
-                        return dataPoints
+        for mod, _, _ in self.activeModules():
+            if mod.connection != connection:
+                continue
+            dataPoints = mod.handleFrame(mod,frame)
+            if dataPoints:
+                return dataPoints
 
         return None
 
@@ -393,41 +382,17 @@ class ExperimentInteractor(QObject):
         data = []
         self.runningExperiment = True
         try:
-            for row in range(self.targetModel.rowCount()):
-                index = self.targetModel.index(row, 0)
+            for mod, name, settings in self.activeModules():
 
-                parent = index.model().itemFromIndex(index)
-                moduleName = parent.data(role=PropertyItem.RawDataRole)
+                self.modSets[name] = cp.copy(settings)
+                vals = list(settings.values())
 
-                for module in getRegisteredExperimentModules():
-                    if module[1] == moduleName:
-                        settings = self.getSettings(parent)
-                        self.modSets[moduleName] = cp.copy(settings)
-                        vals = []
-                        for key, val in settings.items():
-                            if val is not None:
-                                vals.append(val)
+                startParams = mod.getStartParams(mod,vals)
+                data.extend(self.paramsToConnData(startParams, mod.connection))
 
-                        startParams = module[0].getStartParams(module[0], vals)
-                        if startParams is not None:
-                            if isinstance(startParams, list):
-                                for _startParams in startParams:
-                                    _startParams['connection'] = module[0].connection
-                                    data.append(_startParams)
-                            else:
-                                startParams['connection'] = module[0].connection
-                                data.append(startParams)
+                params = mod.getParams(mod,vals)
+                data.extend(self.paramsToConnData(params, mod.connection))
 
-                        params = module[0].getParams(module[0], vals)
-                        if params and not None:
-                            if isinstance(params, list):
-                                for _params in params:
-                                    _params['connection'] = module[0].connection
-                                    data.append(_params)
-                            else:
-                                params['connection'] = module[0].connection
-                                data.append(params)
-                        break
         except ExperimentModuleException as eme:
             self._logger.error(eme)
             self.runningExperiment = False
@@ -448,32 +413,15 @@ class ExperimentInteractor(QObject):
         Sends changed parameters of all modules that are registered in the target model to an experiment.
         """
         data = []
-        for row in range(self.targetModel.rowCount()):
-            index = self.targetModel.index(row, 0)
+        for mod, name, settings in self.activeModules():
 
-            parent = index.model().itemFromIndex(index)
-            moduleName = parent.data(role=PropertyItem.RawDataRole)
-
-            for module in getRegisteredExperimentModules():
-                if module[1] == moduleName:
-                    settings = self.getSettings(parent)
-                    if self.modSets[moduleName] == settings:
-                        break
-                    self.modSets[moduleName] = cp.copy(settings)
-                    vals = []
-                    for key, val in settings.items():
-                        if val is not None:
-                            vals.append(val)
-                    params = module[0].getParams(module[0], vals)
-                    if params and not None:
-                        if isinstance(params, list):
-                            for _params in params:
-                                _params['connection'] = module[0].connection
-                                data.append(_params)
-                        else:
-                            params['connection'] = module[0].connection
-                            data.append(params)
-                    break
+            if self.modSets[name] == settings:
+                print('continuing')
+                continue
+            self.modSets[name] = cp.copy(settings)
+            vals = list(settings.values())
+            params = mod.getParams(mod,vals)
+            data.extend(self.paramsToConnData(params, mod.connection))
 
         for _data in data:
             self.sendData.emit(_data)
@@ -483,29 +431,11 @@ class ExperimentInteractor(QObject):
         Sends all parameters of all modules that are registered in the target model to an experiment.
         """
         data = []
-        for row in range(self.targetModel.rowCount()):
-            index = self.targetModel.index(row, 0)
+        for mod, name, settings in self.activeModules():
 
-            parent = index.model().itemFromIndex(index)
-            moduleName = parent.data(role=PropertyItem.RawDataRole)
-
-            for module in getRegisteredExperimentModules():
-                if module[1] == moduleName:
-                    settings = self.getSettings(parent)
-                    vals = []
-                    for key, val in settings.items():
-                        if val is not None:
-                            vals.append(val)
-                    params = module[0].getParams(module[0], vals)
-                    if params and not None:
-                        if isinstance(params, list):
-                            for _params in params:
-                                _params['connection'] = module[0].connection
-                                data.append(_params)
-                        else:
-                            params['connection'] = module[0].connection
-                            data.append(params)
-                    break
+            vals = list(settings.values())
+            params = mod.getParams(mod,vals)
+            data.extend(self.paramsToConnData(params, mod.connection))
 
         for _data in data:
             self.sendData.emit(_data)
@@ -521,30 +451,12 @@ class ExperimentInteractor(QObject):
             return
 
         self.runningExperiment = False
-        for row in range(self.targetModel.rowCount()):
-            index = self.targetModel.index(row, 0)
+        for mod, name, settings in self.activeModules():
 
-            parent = index.model().itemFromIndex(index)
-            moduleName = parent.data(role=PropertyItem.RawDataRole)
+            vals = list(settings.values())
 
-            for module in getRegisteredExperimentModules():
-                if module[1] == moduleName:
-                    settings = self.getSettings(parent)
-                    vals = []
-                    for key, val in settings.items():
-                        if val is not None:
-                            vals.append(val)
-
-                    stopParams = module[0].getStopParams(module[0], vals)
-                    if stopParams is not None:
-                        if isinstance(stopParams, list):
-                            for _stopParams in stopParams:
-                                _stopParams['connection'] = module[0].connection
-                                data.append(_stopParams)
-                        else:
-                            stopParams['connection'] = module[0].connection
-                            data.append(stopParams)
-                    break
+            stopParams = mod.getStopParams(mod,vals)
+            data.extend(self.paramsToConnData(stopParams, mod.connection))
 
         # stop experiment
         if broadcast:
@@ -564,15 +476,27 @@ class ExperimentInteractor(QObject):
         """
         exp = {'Name': self.targetModel.getName()}
 
-        for row in range(self.targetModel.rowCount()):
-            index = self.targetModel.index(row, 0)
-
-            parent = index.model().itemFromIndex(index)
-            moduleName = parent.data(role=PropertyItem.RawDataRole)
-
-            for module in getRegisteredExperimentModules():
-                if module[1] == moduleName:
-                    exp[moduleName] = self.getSettings(parent)
-                    break
+        for _, name, settings in self.activeModules():
+            exp[name] = settings
 
         return exp
+
+    def activeModules(self):
+        for row in range(self.targetModel.rowCount()):
+            index = self.targetModel.index(row, 0)
+            parent = index.model().itemFromIndex(index)
+            settings = self.getSettings(parent)
+            name = parent.data(role=PropertyItem.RawDataRole)
+            mod = getRegisteredExperimentModules()[name]
+            yield mod, name, settings
+
+    def paramsToConnData(self, params, conn):
+            if not params:
+                return []
+            data = []
+            if not isinstance(params, list):
+                params = [params]
+            for p in params:
+                p['connection'] = conn
+                data.append(p)
+            return data

pywisp/registry.py
@@ -1,37 +1,36 @@
 # -*- coding: utf-8 -*-
-from .experimentModules import *
+from .experimentModules import ExperimentModule
 from .visualization import Visualizer
 from .connection import Connection
 
-_registry = {}
+class Registry(dict):
+    def __init__(self):
+        self[ExperimentModule] = {}
+        self[Connection] = {}
+        self[Visualizer] = {}
 
+    def register(self, type, mod):
+        if not issubclass(mod, type):
+            raise TypeError(f"Cannot register {mod} as {type}!")
+        if mod in self[type].values():
+            raise ValueError(f'class {mod} already registered as {type}')
+        self[type][mod.__name__] = mod
+
+_registry = Registry()
 
 def registerExperimentModule(expCls):
     """
     hook to register a module in the pywisp framework
     :param expCls: class to be registered
     """
-    if not issubclass(expCls, ExperimentModule):
-        raise TypeError("Module must match type to be registered for! "
-                        "{0} <> {1}".format(expCls, ExperimentModule))
-
-    clsEntry = _registry.get(ExperimentModule, [])
-    increment = (expCls, expCls.__name__)
-    if increment in clsEntry:
-        raise ValueError("class {0} already registered as experimentModule!"
-                         "".format(expCls))
-
-    clsEntry.append(increment)
-    _registry[ExperimentModule] = clsEntry
-    _registry[ExperimentModule.__name__] = clsEntry
-
+    _registry.register(ExperimentModule, expCls)
 
 def getRegisteredExperimentModules():
     """
     hook to retrieve registered experiment modules
     :return: list of experiment modules
     """
-    return _registry.get(ExperimentModule, [])
+    return _registry[ExperimentModule]
 
 
 def registerConnection(connCls):
@@ -39,27 +38,14 @@ def registerConnection(connCls):
     hook to register a connection in the pywisp framework
     :param connCls: class to be registered
     """
-    if not issubclass(connCls, Connection):
-        raise TypeError("Module must match type to be registered for! "
-                        "{0} <> {1}".format(connCls, Connection))
-
-    clsEntry = _registry.get(Connection, [])
-    increment = (connCls, connCls.__name__)
-    if increment in clsEntry:
-        raise ValueError("class {0} already registered as connection!"
-                         "".format(connCls))
-
-    clsEntry.append(increment)
-    _registry[Connection] = clsEntry
-    _registry[Connection.__name__] = clsEntry
-
+    _registry.register(Connection, connCls)
 
 def getRegisteredConnections():
     """
     hook to retrieve registered connections
     :return: list of connection classes
     """
-    return _registry.get(Connection, [])
+    return _registry[Connection]
 
 
 def registerVisualizer(visCls):
@@ -67,24 +53,11 @@ def registerVisualizer(visCls):
     hook to register a visualizer for the experiment GUI
     :param visCls: class to be registered
     """
-    if not issubclass(visCls, Visualizer):
-        raise TypeError("Module must match type to be registered for! "
-                        "{0} <> {1}".format(visCls, Visualizer))
-
-    clsEntry = _registry.get(Visualizer, [])
-    increment = (visCls, visCls.__name__)
-    if increment in clsEntry:
-        raise ValueError("class {0} already registered as visualizer!"
-                         "".format(visCls))
-
-    clsEntry.append(increment)
-    _registry[Visualizer] = clsEntry
-    _registry[Visualizer.__name__] = clsEntry
-
+    _registry.register(Visualizer, visCls)
 
 def getRegisteredVisualizers():
     """
     hook to retrieve registered visualizers
     :return: list of visualizer classes
     """
-    return _registry.get(Visualizer, [])
+    return _registry[Visualizer]

commit e3910a5739b81ba8f966d058218ecdda7d0187fe

disallow switching during running experiment
changing to a different experiment and loading past measurements now
checks that no experiment is currently running.

fixes a standing bug introduced in 8512bcd

pywisp/gui.py
@@ -1129,6 +1129,9 @@ class MainGui(QMainWindow):
         Apply the selected experiment to the current target and set it bold.
         :param item: item of the experiment in the `ExperimentList`
         """
+        if self.isConnected and self.actStopExperiment.isEnabled():
+            self._logger.info("stop the running experiment to change experiments")
+            return
         idx = self.experimentList.row(item)
         success = self._applyExperimentByIdx(idx)
         self._currentExpListItem = item
@@ -1354,6 +1357,9 @@ class MainGui(QMainWindow):
         Loads the measurement data from the measurement dict by given item from last measurement list
         :param item: last measurement list item
         """
+        if self.isConnected and self.actStopExperiment.isEnabled():
+            self._logger.info("stop the running experiment to view past measurements")
+            return
         self.actStartExperiment.setDisabled(True)
         self.selectedExp = False
         self.remoteWidgetLayout.clearAll()