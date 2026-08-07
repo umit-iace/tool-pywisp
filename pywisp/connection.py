b31291215d3f7c6b246f26cdcc622543d0ecb30c
Jonathan Halmen <slowjo@halmen.xyz>
2026-08-07 09:08:10 +0200

    remove IACEConnection
    closes #165

--- pywisp/connection.py
+++ pywisp/connection.py
@@ -13,7 +13,7 @@ from PyQt5.QtCore import QObject, QThread, pyqtSignal
 from .min import Packer, Unpacker, Frame, Bytewise, HDRStuf
 from .utils import coroutine, pipe
 
-__all__ = ["Connection", "UdpConnection", "TcpConnection", "SerialConnection", "IACEConnection"]
+__all__ = ["Connection", "UdpConnection", "TcpConnection", "SerialConnection"]
 
 
 class ConnReader(QObject):
@@ -250,7 +250,3 @@ class UdpConnection(SocketConnection):
     def __init__(self, ip, port, **kwargs):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         super().__init__(sock, ip, port, tx=Packer, rx=[Bytewise, HDRStuf, Unpacker], **kwargs)
-
-class IACEConnection(UdpConnection):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
